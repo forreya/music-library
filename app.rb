c2522b7e44e104071fda67d53b3c04b29abeaf9a
Ryan Lai <ryanlai12@gmail.com>
2023-01-30 15:20:38 +0000

    Wrote small program in 'app.rb' using the class AlbumRepository to print out the list of albums to the terminal

--- app.rb
+++ app.rb
@@ -1,12 +1,19 @@
 
 require_relative 'lib/database_connection'
 require_relative 'lib/artist_repository'
+require_relative 'lib/album_repository'
 
 DatabaseConnection.connect('music_library')
 
 artist_repository = ArtistRepository.new
+album_repository = AlbumRepository.new
 
-artist_repository.all.each {
-  |artist|
-  p artist
+# artist_repository.all.each {
+#   |artist|
+#   p artist
+# }
+
+album_repository.all.each{
+  |album|
+  p album
 }
